389f1ff402d0abbb23cb7974779e333b8cd32784
Arnav Shah <arnav404.ctf@gmail.com>
2025-12-11 09:56:52 +0530

    Non Misc updates

---
+++ README.md
@@ -0,0 +1,3 @@
+# learn-c
+
+This repository contains my practice code while learning the C programming language.
---
+++ basics.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int a = 5;
+    int b = 10;
+    printf("Sum = %d\n", a + b);
+    return 0;
+}
+// Updated comments for clarity
